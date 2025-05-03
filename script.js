6caf58142dd97075731d42e0a98617b9fb42825e
von-solano <a1890523@adelaide.edu.au>
2025-05-03 23:35:23 +0930

    feat: creating function to score password based on length and character variety (incomplete)

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// regular expressions for password char types
+var lowercase = /[a-z]/;
+var uppercase = /[A-Z]/;
+var numbers = /[0-9]/;
+var special_chars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+
 
 // function to toggle password visibility onclick()
 function toggle_visibility() {
@@ -11,4 +17,31 @@ function toggle_visibility() {
         password_input.type = "password";  // swap input type to password (hides password)
         toggle_button.textContent = "Show" // swap text on button to "Show"
     }
+}
+
+// function to score password strength
+function score_password(){
+    const password_input = document.getElementById("password"); // get password element
+    const password = password_input.value; // value of password
+    let score = 0; // score variable
+
+    // test password length
+    if(password.length >= 12){
+        score +=2; // +2 points for password length
+    } else if (password.length >= 8){
+        score++; // +1 point for 8 characters
+    } else {
+        return score = 0; // password is too short, return lowest score
+    }
+
+    // test password inputs: 
+    // checks character variety
+    if(uppercase.test(password)) score++;
+    if(lowercase.test(password)) score++;
+    if(numbers.test(password)) score++;
+    if(special_chars.test(password)) score++;
+
+    // TEST: see score output * temporary *
+    const text = document.getElementById("disclaimer");
+    text.textContent = `Strength Score: ${score}`;
 }
