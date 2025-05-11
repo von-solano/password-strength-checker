4978d93399160201a1fdb1ae7ae1d65a9758b184
von-solano <a1890523@adelaide.edu.au>
2025-05-11 11:03:25 +0930

    feat: added basic common password dictionary and implemented strength meter based on score

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // regular expressions for password char types
-var lowercase = /[a-z]/;
-var uppercase = /[A-Z]/;
-var numbers = /[0-9]/;
-var special_chars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+let lowercase = /[a-z]/;
+let uppercase = /[A-Z]/;
+let numbers = /[0-9]/;
+let special_chars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
 
 
 // function to toggle password visibility onclick()
@@ -20,15 +20,24 @@ function toggle_visibility() {
 }
 
 // function to score password strength
-function score_password(){
+function score_password() {
     const password_input = document.getElementById("password"); // get password element
     const password = password_input.value; // value of password
     let score = 0; // score variable
 
+    // create array of common/weak passwords
+    const common_passwords = ["password", "password123", "12345678", "123456789", "qwerty", "qwerty1", "qwerty123", "abc123", "11111111", "123123123", "guest", "secret"];
+    const password_lower = password.toLowerCase(); // turn password to lowercase
+
+    // check if password contains common/weak patterns
+    if (common_passwords.some(bad => password_lower.includes(bad))) {
+        return score = 0; // password is common, return lowest score
+    }
+
     // test password length
-    if(password.length >= 12){
-        score +=2; // +2 points for password length
-    } else if (password.length >= 8){
+    if (password.length >= 12) {
+        score += 2; // +2 points for password length
+    } else if (password.length >= 8) {
         score++; // +1 point for 8 characters
     } else {
         return score = 0; // password is too short, return lowest score
@@ -36,12 +45,27 @@ function score_password(){
 
     // test password inputs: 
     // checks character variety
-    if(uppercase.test(password)) score++;
-    if(lowercase.test(password)) score++;
-    if(numbers.test(password)) score++;
-    if(special_chars.test(password)) score++;
+    if (uppercase.test(password)) score++;
+    if (lowercase.test(password)) score++;
+    if (numbers.test(password)) score++;
+    if (special_chars.test(password)) score++;
 
-    // TEST: see score output * temporary *
-    const text = document.getElementById("disclaimer");
-    text.textContent = `Strength Score: ${score}`;
+    return score;
+}
+
+// function to update strength bar based on score
+function update_meter(score) {
+    let strength_meter = document.getElementById("strength_meter");
+
+    // update bar based on score
+    if (score >= 6) {
+        strength_meter.className = ""; // reset class 
+        strength_meter.classList.add("strong")
+    } else if (score == 5) {
+        strength_meter.className = ""; // reset class 
+        strength_meter.classList.add("moderate")
+    } else {
+        strength_meter.className = ""; // reset class 
+        strength_meter.classList.add("weak")
+    }
 }
